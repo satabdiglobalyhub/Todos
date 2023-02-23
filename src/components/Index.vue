be34436ddf6e68e3e99ef0b322e7110c3d1d0deb
satabdiglobalyhub <satabdi.gautam@globalyhub.com>
2023-02-23 14:14:28 +0545

    components created

---
+++ src/components/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <TodoInput />
+  <Todolist />
+  <TodoButton />
+</template>
+<script>
+import TodoInput from "../UI/components/TodoInput.vue";
+import Todolist from "./Todoapp/components/Todolist.vue";
+import TodoButton from "../UI/components/TodoButton.vue";
+export default {
+  components: {
+    TodoInput,
+    Todolist,
+    TodoButton,
+  },
+};
+</script>
+<style></style>
---
+++ src/components/Todoapp/components/Todolist.vue
@@ -0,0 +1,7 @@
+<template>todolists</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
