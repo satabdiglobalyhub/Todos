ebd0fc9ab1609b3f4df0c2ed9a3ffb22f3106131
satabdiglobalyhub <satabdi.gautam@globalyhub.com>
2023-02-24 14:55:16 +0545

    feat:component integration

--- src/components/Todoapp/components/Todolist.vue
+++ src/components/Todoapp/components/Todolist.vue
@@ -1,6 +1,6 @@
 <template>
-  <div >
-    <ul class="list">
+  <div class="list">
+    <ul >
       <li>static todolist <TodoButton :buttonName="Delete" /></li>
     </ul>
   </div>
@@ -21,11 +21,11 @@ export default {
 </script>
 
 <style>
-
 .list {
   padding: 10px;
   border-bottom: 3px solid #2c3e50;
   font-size: xx-large;
   display: flex;
+  margin: 0px 20px;
 }
 </style>
